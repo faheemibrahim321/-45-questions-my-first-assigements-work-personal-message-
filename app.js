009133167a681ec1b25430e46b0c7f0ac0e45d3e
faheemibrahim321 <161450679+faheemibrahim321@users.noreply.github.com>
2024-02-29 21:57:43 +0500

    45 questions my first assigements work
    -45-questions-my-first-assigements-work-personal-message-

---
+++ app.js
@@ -0,0 +1,3 @@
+"use strict";
+let personName = "Hello Eric";
+console.log(`hello ${personName}would you like to learn some Python today?`);
---
+++ app.ts
@@ -0,0 +1,2 @@
+let personName:string ="Hello Eric";
+console.log(`hello ${personName}would you like to learn some Python today?`)
